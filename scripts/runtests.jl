4a5984399da41f7219cf330c3f5d345f1392b524
Luiz-Rafael Santos <lrsantos11@gmail.com>
2021-02-04 18:04:33 -0300

    adds inclusion of BBS20 plots and results

--- scripts/runtests.jl
+++ scripts/runtests.jl
@@ -2,7 +2,6 @@ using DrWatson
 @quickactivate "CRM-CFP"
 include(srcdir("CRM-CFP.jl"))
 
-# Make Plots
-# include("makeplots_ABBIS20.jl")
-
+include("makeplots_ABBIS20.jl")
 include("makeplots_BBS20.jl")
+
