1f5c7e0814d4e1e1768207aef6778bec08da1372
Luiz-Rafael Santos <lrsantos11@gmail.com>
2021-10-06 17:37:16 -0300

    Adds Simultaneous Projections Method (SPM)

--- src/CRM-CFP.jl
+++ src/CRM-CFP.jl
@@ -3,5 +3,6 @@ include("crm_utils.jl")
 global const ZERO_VAL = 1e-15
 include("MAP.jl")
 include("DRM.jl")
+include("SPM.jl")
 include("CRM.jl")
 include("CentCRM.jl")
---
+++ src/SPM.jl
@@ -0,0 +1,64 @@
+"""
+    SPMiteration!(xSPM,ProjectA,ProjectB)
+
+Computes a SPM iteration
+"""
+function SPM_iteration!(xSPM::Vector, 
+                        ProjectionA::Function, 
+                        ProjectionB::Function)
+    xSPM = 0.5*(ProjectionA(xSPM) + ProjectionB(xSPM))                       
+    return xSPM
+end 
+
+"""
+    SPMiteration!(xSPM,ProjA,ProjectB)
+
+Computes a SPM iteration
+"""
+function SPM_iteration!(xSPM::Vector, 
+                        ProjA::Vector, 
+                        ProjectionB::Function)
+    xSPM = 0.5*(ProjA + ProjectionB(xSPM))                       
+    return xSPM
+end 
+
+
+
+"""
+    SPM(x₀,ProjectA, ProjectB)
+
+    Simultaneous Projections Method
+"""
+function SPM(x₀::Vector, ProjectA::Function, ProjectB::Function; 
+            EPSVAL::Float64 = 1e-5,
+            itmax::Int = 100,
+            filedir::String = "",
+            xSol::Vector = [],
+            print_intermediate::Bool=true,
+            gap_distance::Bool=false, 
+            isprod::Bool = false)
+    k = 0
+    tolSPM = 1.
+    xSPM = x₀
+    printOnFile(filedir,k, tolSPM, xSPM , deletefile=true, isprod=isprod)
+    ProjA = ProjectA(xSPM)
+    while tolSPM > EPSVAL && k < itmax
+        print_intermediate ? printOnFile(filedir, 0, 0., ProjA , isprod=isprod) : nothing
+        if gap_distance
+            xSPM  = SPM_iteration!(xSPM, ProjA, ProjectB)
+            ProjA = ProjectA(xSPM)
+            tolSPM = norm(ProjA-xSPM)
+        else
+            xSPMOld = copy(xSPM)
+            xSPM  = SPM_iteration!(xSPM, ProjA, ProjectB)
+            ProjA = ProjectA(xSPM)
+            tolSPM = Tolerance(xSPM,xSPMOld,xSol)
+        end
+        k += 2
+        printOnFile(filedir,k, tolSPM, xSPM , isprod=isprod)
+    end
+    isprod ? method = :SPMprod : method = :SPM
+    return Results(iter_total= k,final_tol=tolSPM,xApprox=xSPM,method=method)
+end
+
+
