135bdd646b34c5f9c6e5fd05c782da340bc241c3
Luiz-Rafael Santos <lrsantos11@gmail.com>
2021-10-12 12:15:33 -0300

    updates PlotUtils

--- scripts/plots_util.jl
+++ scripts/plots_util.jl
@@ -1,34 +1,35 @@
 """
 Creates plot with the path of method. 
 """
-function MethodPath!(plt::Plots.Plot,mat::Array;
-    ltype = :dash, lwidth=.5, color=:dodgerblue2,alpha::Float64=1.0, λ::Float64 = .97, arrow = :closed)
+function MethodPath!(plt::Plots.Plot,mat::Array; λ::Float64 = .97,  kwargs...)
     x = @view mat[:,1]
     y = @view mat[:,2]
     num_arrows = length(x)-1
     for index = 1:num_arrows
         x2 = x[index] + λ*(x[index+1] - x[index])
         y2 = y[index] + λ*(y[index+1] - y[index])
-        plot!(plt,[x[index],x2],[y[index],y2],line=(:path,alpha),ls=ltype,c=color,label="",lw=lwidth,arrow=arrow)
+        plot!(plt,[x[index],x2],[y[index],y2], lt = :path,  label="", arrow = :closed, ls = :dash; kwargs...)
     end
     return plt
 end
 
 function label_points!(plt::Plots.Plot,
                        mat::Vector;
-                       num_points::Int = 5,
-                       var_name::String = "z",
+                       num_points::Int = 1,
+                       var_name::String = "z^",
+                       var_indices::Vector{Int} = Int[],
                        label_size::Int = 8,
-                       shift::Float64 = 0.05)
-    list_pts = @view mat[1:num_points] 
+                       xshift::Real = 0.0,
+                       yshift::Real = 0.0,
+                       kwargs...)
+    list_pts = @view mat[1:num_points]
+    if isempty(var_indices)
+         var_indices = collect(0:num_points -1)
+    end
     for pto in eachrow(list_pts)
         ptox = pto[1][1]
         ptoy = pto[1][2]
-        if isodd(pto.indices[1])
-            annotate!(plt,[(ptox, ptoy+shift ,text(L"%$(var_name)_{%$(pto.indices[1] - 1)}",label_size))])
-        else
-            annotate!(plt,[(ptox, ptoy-shift ,text(L"%$(var_name)_{%$(pto.indices[1] - 1)}",label_size))])
-        end    
+        annotate!(plt,[(ptox+xshift, ptoy+yshift,text(L"%$(var_name){%$(var_indices[pto.indices[1]])}",label_size))];kwargs...)
     end
     return plt
 end
