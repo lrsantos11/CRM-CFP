2fd484f4ebf27f1b70af13437d42c7722495f483
Luiz-Rafael Santos <lrsantos11@gmail.com>
2023-03-24 08:48:02 -0700

    Move plots_util.jl

--- src/CRM-CFP.jl
+++ src/CRM-CFP.jl
@@ -1,6 +1,7 @@
 __precompile__()
-global const ZERO_VAL = 1e-15
+const global ZERO_VAL = 1e-15
 include("crm_utils.jl")
+include("plots_util.jl")
 include("read_netlib_MPS.jl")
 include("MAP.jl")
 include("DRM.jl")
