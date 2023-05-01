f9a27d8ee2c19f8da2c1738b4fb5535a7a872b5b
Luiz-Rafael Santos <lrsantos11@gmail.com>
2023-05-01 08:43:16 -0700

    creates BBILS23 structure

---
+++ scripts/BBILS23_runtests.jl
@@ -0,0 +1,16 @@
+__precompile__
+using DrWatson
+@quickactivate "CRM-CFP"
+include(srcdir("CRM-CFP.jl"))
+
+# pgfplotsx()
+# include(scriptsdir("plots_util.jl"))
+
+"""juliadocs
+
+[^Behling2023a] R. Behling, J.-Y. Bello-Cruz, A. N. Iusem, D. Liu e L.-R. Santos, A successive centralized circumcentered-reflection method for the convex feasibility problem.
+"""
+##
+
+##
+# include(scriptsdir("BBILS23_Ellipsoids.jl"))
