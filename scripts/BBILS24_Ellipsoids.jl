d4796608798875afe6e75c5bf0c0e57dcac550c4
Luiz-Rafael Santos <lrsantos11@gmail.com>
2023-06-12 14:06:14 -0700

    move Ellipsoids sampling to Ellipsoids_utils.jl

--- scripts/BBILS24_Ellipsoids.jl
+++ scripts/BBILS24_Ellipsoids.jl
@@ -1,156 +1,18 @@
 ##################################################################
-### Creating Ellipsoids Samples
-##################################################################
-
-"""
-SampleTwoEllipsoids(n, p; λ=1.1, γ=1.5)
-Creates two ellipsoids in ℝⁿ that intersect. The intersection is regulated by λ.
-
-"""
-function SampleTwoEllipsoids(n::Int,  # dimension
-    p::Real; # sparsity of matrix A
-    λ::Real=1.1, # parameter for touching ellipsoid
-    γ::Real=1.5 # parameter for making A positive definite
-)
-    Ellipsoids = EllipsoidCRM[]
-    A = Matrix(sprandn(n, n, p))
-    A = (γ * I + A' * A)
-    a = rand(n)
-    b = A * a
-    adotAa = dot(a, b)
-    b .*= -1.0
-    α = (1 + γ) * adotAa
-    push!(Ellipsoids, EllipsoidCRM(A, b, α))
-    TouchEll, Center2, TouchPoint = GenerateTouchingEllipsoid(Ellipsoids[1], n, λ=λ)
-    push!(Ellipsoids, TouchEll)
-    return Ellipsoids, Center2, TouchPoint
-end
-
-
-
-
-"""
-SampleEllipsoids(n, m, p; λ=1.1, γ=1.5)
-Creates m ellipsoids in ℝⁿ that intersect. The intersection is regulated by λ.
-
-"""
-function SampleEllipsoids(n::Int,  # dimension
-    m::Int,  # number of ellipsoids
-    p::Real; # sparsity of matrix A
-    λ::Real=1.1, # parameter for touching ellipsoid
-    γ::Real=1.5 # parameter for making A positive definite
-)
-    Ellipsoids, CenterEll2, TouchPoint = SampleTwoEllipsoids(n, p, λ=λ, γ=γ)
-    point_inter = 0.5 * ((1 - λ)CenterEll2 + (1 + λ)TouchPoint)
-    for _ in 3:m
-        center = randn(n)
-        while any(Ref(center) .∈ Ellipsoids)
-            center *= 1.5
-        end
-        d = λ * (point_inter - center)
-        push!(Ellipsoids, GenerateEllipsoid(center, d))
-    end
-    return Ellipsoids, point_inter
-end
-
-"""
-    GenerateTouchingEllipsoid(ell, n; λ=1.0)
-    Given an ellipsoid ell, generates a touching ellipsoid in ℝⁿ
-
-    """
-
-
-function GenerateTouchingEllipsoid(ell::EllipsoidCRM,
-    n::Int;
-    λ::Real=1.1)
-    c = randn(n)
-    while c ∈ ell
-        c *= 1.5
-    end
-    x̂ = Proj_Ellipsoid(c, ell)
-    d = λ * (x̂ - c)
-    return GenerateEllipsoid(c, d), c, x̂
-end
-
-
-
-"""
- GenerateEllipsoid(center, semi_axis)
- Given center and larger semi_axis, generates an ellipsoid in ℝⁿ
-"""
-
-function GenerateEllipsoid(center::Vector,
-    semi_axis::Vector)
-    n = length(center)
-    semi_axis_norm = norm(semi_axis)
-    Λ = Diagonal([semi_axis_norm; semi_axis_norm .+ 2 * rand(n - 1)])
-    # Λ = Diagonal([semi_axis_norm; rand(n - 1)* .8 * semi_axis_norm])   
-    Q, _ = qr(randn(n, n))
-    M2 = Q' * Λ .^ 2 * Q
-    return EllipsoidCRM(center, 0.5(M2 + M2'))
-end
-
-
-
-##
-##################################################################
 ### Methods
 ##################################################################
 
 
 """
-    SucCentCRM_Cyclic(x₀, Ellipsoids)
+    PACA(x₀, Ellipsoids)
 
-Successive Centralized Cirumcentered-Reflection method using Cyclic control sequence
+Perturbed Approximate Circumcenter Algorithm
 """
-function SucCentCRM_Cyclic(x₀::Vector,
+function PACA(x₀::Vector,
     Ellipsoids::Vector{EllipsoidCRM}; kargs...)
     Projections = Function[x -> Proj_Ellipsoid(x, ell) for ell in Ellipsoids]
-    return SucCentCRM_Cyclic(x₀, Projections; kargs...)
+    return PACA(x₀, Projections; kargs...)
 end
-
-"""
-    SucCentCRM_AlmostViolatedDist(x₀, Ellipsoids)
-"""
-function SucCentCRM_AlmostViolatedDist(x₀::Vector,
-    Ellipsoids::Vector{EllipsoidCRM}; kargs...)
-    Projections = Function[x -> Proj_Ellipsoid(x, ell) for ell in Ellipsoids]
-    return SucCentCRM_AlmostViolatedDist(x₀, Projections; kargs...)
-end
-
-"""
-    SucCentCRM_AlmostViolatedFunc(x₀, Ellipsoids)
-"""
-function SucCentCRM_AlmostViolatedFunc(x₀::Vector,
-    Ellipsoids::Vector{EllipsoidCRM}; kargs...)
-    Projections = Function[x -> Proj_Ellipsoid(x, ell) for ell in Ellipsoids]
-    FunctionEval = Function[x -> func_EllipsoidCRM(x, ell) for ell in Ellipsoids]
-    return SucCentCRM_AlmostViolatedFunc(x₀, Projections, FunctionEval; kargs...)
-end
-
-
-"""
-    SePM(x₀, Ellipsoids)
-"""
-function SePM(x₀::Vector,
-    Ellipsoids::Vector{EllipsoidCRM}; kargs...)
-    Projections = Function[x -> Proj_Ellipsoid(x, ell) for ell in Ellipsoids]
-    return SePM(x₀, Projections; kargs...)
-end
-
-"""
-    CRMProd(x₀, Ellipsoids)
-"""
-function CRMprod(x₀::Vector,
-    Ellipsoids::Vector{EllipsoidCRM}; kargs...)
-    m = length(Ellipsoids)
-    Projections = Function[x -> Proj_Ellipsoid(x, ell) for ell in Ellipsoids]
-    Results = CRMprod(x₀, Projections; kargs...)
-    Results.iter_total = div(Results.iter_total, 2)
-    Results.proj_total = m * Results.iter_total
-    return Results
-end
-
 
 
 ##
